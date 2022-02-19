702bcfb9381fabfda0980c2dcadc922c0f9666ea
jyotijauhari <jyotijauhari222@gmail.com>
2022-02-20 00:06:07 +0530

    automatic type conversion

---
+++ js/automaticTypeConversion.js
@@ -0,0 +1,21 @@
+var ans = "60" + 7;
+console.log(ans);
+
+var ans = 4 + "1";
+console.log(ans);
+
+//undefined and null are almost same
+
+// undefined * something = NaN
+// null * something = 0
+
+
+// plus is sign of addition and concat
+// but - is substraction -> so string become number 
+console.log("5"-1);  // 4
+
+console.log("a"-1);  // NaN
+
+console.log(null*"5"); //0
+
+console.log("ten"*5); //NaN
