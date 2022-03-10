ff99095ea57b45d76bd90f76e8a2b5881b0d70a4
jyotijauhari <jyotijauhari222@gmail.com>
2022-03-10 23:20:27 +0530

    -b and -n options added

--- Node/wcat/wcat.js
+++ Node/wcat/wcat.js
@@ -43,9 +43,10 @@ console.log(content);
 
 //in windows \r\n is there (maybe be because of enter 'r', just remember \r\n is there instead of \n for new line)
 let contentArr = content.split("\r\n"); //in mac -> \n
-console.table(contentArr);
+// console.table(contentArr);
 
 //check if -s is present or not
+let tempArr = [];
 let isSPresent = optionsArr.includes("-s");
 if (isSPresent) {
     for (let i = 1; i < contentArr.length; i++){
@@ -57,16 +58,74 @@ if (isSPresent) {
         }
     }
     // arr with multiple '' replaced by null 
-    console.table(contentArr);
-    
-    let tempArr = [];
+    // console.table(contentArr);
+
     //push everything in tempArr except null
     for (let i = 0; i < contentArr.length; i++){
         if (contentArr[i] != null) {
             tempArr.push(contentArr[i]);
         }
     }
+    // console.log("data after removing extra line (-s option)" + tempArr);
     console.log(tempArr);
+}
+contentArr = tempArr;
+
+
+
+//getting idx of -n and -b
+let indexofN = optionsArr.indexOf("-n");
+let indexofB = optionsArr.indexOf("-b");
+//of -n or -b is not found, -1 is returned
+
+//deciding final option
+let finalOption = "";
+if(indexofN != -1 && indexofB != -1){
+    if(indexofN < indexofB){
+        finalOption = "-n";
+    }
+    else{
+        finalOption = "-b";
+    }
+}
+else{
+    if(indexofN != -1){
+        finalOption = "-n";
+    }
+    else if(indexofB != -1){
+        finalOption = "-b";
+    }
 }
 
 
+//evaluating -b or -n depending upon final option
+if(finalOption == "-n"){
+    modifyContentByN();
+}
+else if(finalOption == "-b"){
+    modifyContentByB();
+}
+
+function modifyContentByN(){
+    let index = 1;
+    for(let i=0; i<contentArr.length;i++){
+        contentArr[i] = index + ")"+ contentArr[i];
+        index += 1;
+    }
+}
+
+function modifyContentByB(){
+    let index = 1;
+    for(let i=0; i<contentArr.length;i++){
+        if (contentArr[i] == "" ) {
+            contentArr[i] = contentArr[i];
+        }
+        else{
+            contentArr[i] = index + ")" + contentArr[i];
+            index += 1;
+        }
+    }
+}
+
+//printing final output
+ console.log(contentArr);
