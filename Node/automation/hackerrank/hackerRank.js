3a15f80ab00b5c2afc883b7a2b70fd170540a8d9
jyotijauhari <jyotijauhari222@gmail.com>
2022-04-03 05:03:59 +0530

    :rocket: v1.0 hackerRank automation

--- Node/automation/hackerrank/hackerRank.js
+++ Node/automation/hackerrank/hackerRank.js
@@ -54,7 +54,35 @@ browserOpenPromise
         return alogorithmTabWillBeOpenedPromise;
     })
     .then(function(){
-        console.log("algorithm page opened.. "); // ***how this is printed first even before prev one??
+        console.log("algorithm page opened.. "); // ***how this is printed first even before prev one?? -> lol you returned wrong promise from the prev function :)
+        let allQuesPromise = cTab.waitForSelector('a[data-analytics="ChallengeListChallengeName"]'); //waiting for all ques to get loaded
+        return allQuesPromise;
+    })
+    .then(function(){
+        // function which return all ques links
+        function getAllQuesLinks(){
+            let allElemArr = document.querySelectorAll(
+                'a[data-analytics="ChallengeListChallengeName"]'
+              );
+            let linksArr = [];
+            for(let i=0; i<allElemArr.length; i++){
+                linksArr.push(allElemArr[i].getAttribute("href"));
+            }
+            return linksArr;
+        }
+        let linksArrPromise = cTab.evaluate(getAllQuesLinks); // linksArrPromise will receive promise object // .evaluate have inbuilt the resolve and reject method, as inbuilt promise it is 
+        return linksArrPromise;
+    })
+    .then(function(linksArr){
+        console.log("links to all ques received: ");
+        // console.log(linksArr);
+
+        //now solving que
+        let questionsWillBeSolvedPromise = questionSolver(linksArr[0], 0);
+        return questionsWillBeSolvedPromise;
+    })
+    .then(function(){
+        console.log("Tada questiion is solved.");
     })
 
     .catch(function(err){
@@ -77,10 +105,121 @@ browserOpenPromise
                 })
                 .then(function(){
                     console.log("algo button is clicked...");
+                    resolve();
                 })
                 .catch(function(err){
                     console.log(err);
                 });
-            return waitForSelectorPromise;
-        })
+        });
+        return waitThenClickPromise;
     }
+
+function questionSolver(url, idx){
+    let questionWillBeSolvedbyFunctionPromise = new Promise(function (resolve, reject){
+        let fullLink = `https://www.hackerrank.com${url}`;
+        let goToQuesPagePromise = cTab.goto(fullLink);
+        goToQuesPagePromise
+        .then(function(){
+            console.log("question opened woahoo.");
+            //tick the custome input box mark
+            let waitForCheckBoxAndClickPromise = waitAndClick(".checkbox-input");
+            return waitForCheckBoxAndClickPromise;
+        })
+        .then(function(){
+            console.log("tick box clicked");
+            //select box where code will be typed
+            let waitForTextBoxPromise = cTab.waitForSelector(".custominput");
+            return waitForTextBoxPromise;
+        })
+        .then(function(){
+            console.log("text box loaded");
+
+            //type code in text box
+            let code1 = `#include <cmath>
+            #include <cstdio>
+            #include <vector>
+            #include <iostream>
+            #include <algorithm>
+            using namespace std;
+            
+            
+            int solveMeFirst(int a, int b) {
+              return a+b;
+            }
+            int main() {
+              int num1, num2;
+              int sum;
+              cin>>num1>>num2;
+              sum = solveMeFirst(num1,num2);
+              cout<<sum;
+              return 0;
+            }`;
+
+            let codeWillBeTypedPromise = cTab.type( ".custominput" ,code1);
+            return codeWillBeTypedPromise;
+        })
+        .then(function(){
+            console.log("code typed in custome text box....")
+            //press ctrl 
+            let controlKeyWillBePressedPromised = cTab.keyboard.down("Control");
+            return controlKeyWillBePressedPromised;
+        })
+        .then(function(){
+            console.log("ctrl key pressed...")
+            //press a 
+            let aKeyWillBePressedPromised = cTab.keyboard.press("a");
+            return aKeyWillBePressedPromised;
+        })
+        .then(function(){
+            console.log("a key pressed...")
+            //press x 
+            let xKeyWillBePressedPromised = cTab.keyboard.press("x");
+            return xKeyWillBePressedPromised;
+        })
+        .then(function(){
+            console.log("x key pressed");
+            //clicking the editor promise
+            let cursorInEditorPromise = cTab.click(".monaco-editor.no-user-select");
+            return cursorInEditorPromise;
+        })
+        //pasting
+        .then(function(){
+            console.log("clicked editor ...")
+            //press ctrl 
+            let controlKeyWillBePressedPromised = cTab.keyboard.down("Control");
+            return controlKeyWillBePressedPromised;
+        })
+        .then(function () {
+            console.log("clicked ctrl ...")
+            //press a
+            let aKeyPressedPromise = cTab.keyboard.press("a");
+            return aKeyPressedPromise;
+          })
+        .then(function(){
+            console.log("a clicked");
+            //press V
+            let vKeyWillBePressedPromised = cTab.keyboard.press("v");
+            return vKeyWillBePressedPromised;
+        })
+        .then(function () {
+            console.log("v button clicked--> pasted the code");
+            //click submit button
+            let submitButtonClickedPromise = cTab.click(".hr-monaco-submit");
+            return submitButtonClickedPromise;
+        })
+        .then(function(){
+            console.log("submit button clicked");
+            // unclick ctrl key
+            let ControlDownPromise = cTab.keyboard.up("Control");
+            return ControlDownPromise;
+        })
+        .then(function(){
+            console.log("code submitted successfully");
+            resolve();
+        })
+        .catch(function(err){
+            reject(err);
+        });
+    });
+    return questionWillBeSolvedbyFunctionPromise;
+}
