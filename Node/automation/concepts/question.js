afda15e3529bd5d53127eb392fd7944f461197f1
jyotijauhari <jyotijauhari222@gmail.com>
2022-04-16 16:44:09 +0530

    interview ques - var 1 to 10 print with interval of 2sec

--- Node/automation/concepts/question.js
+++ Node/automation/concepts/question.js
@@ -1,5 +1,17 @@
-//print 1 to 10 , each after 2 sec
+//QUESTION -> print 1 to 10 , each after 2 sec
 // this will print from 1 to 10 with interval of 2sec
+
+//DOESNT WORK->
+// 11 will be printed 10 times by this
+// for(var i=0; i<=10; i++){
+//     setTimeout(
+//         function () {
+//             console.log(i);
+//         }
+//     ,2000);
+// }
+
+// way 1
 // for(let i=0; i<=10; i++){
 //     setTimeout(
 //         function () {
@@ -10,6 +22,33 @@
 
 //challenge : for loop and using var print 1-10 with interval of 2sec
 
+// way 2
+// for(var i=0; i<=10; i++){
+//     print(i);
+// }
+
+// function print(i){
+//     setTimeout(
+//         function (){
+//             console.log(i);
+//         }
+//         ,2000*i);
+// }
+
+
+// way 3
+for(var i=1; i<=10; i++){
+    function outer(a){
+        setTimeout(
+            function(){
+                console.log(a);
+            },2000*a
+        );
+    }
+    outer(i);
+}
+
+//WAY 4 - USING RECURSION
 // this will print from 1 to 10 with interval of 2sec
 // var i = 1;
 // function cb(){
@@ -23,27 +62,3 @@
 
 // setTimeout(cb,2000);
 
-
-
-
-// 11 will be printed 10 times by this
-// for(var i=0; i<=10; i++){
-//     setTimeout(
-//         function () {
-//             console.log(i);
-//         }
-//     ,2000);
-// }
-
-
-for(var i=0; i<=10; i++){
-    print(i);
-}
-
-function print(i){
-    setTimeout(
-        function (){
-            console.log(i);
-        }
-        ,2000*i);
-}
