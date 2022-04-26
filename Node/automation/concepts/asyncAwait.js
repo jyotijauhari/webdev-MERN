dc051610ed21415f3fe0b275a90580c228a4d632
jyotijauhari <jyotijauhari222@gmail.com>
2022-04-27 00:07:00 +0530

    :zap: async/await

---
+++ Node/automation/concepts/asyncAwait.js
@@ -0,0 +1,84 @@
+//javascript.info website
+
+const { log } = require("console");
+
+// let's see promises first ->
+function fp() { 
+    return new Promise(function (resolve, reject) {
+      resolve("hi");
+    });
+  }
+  
+  let promisifyFunc = fp();
+  
+  promisifyFunc.then(function (data) {
+    console.log(data);
+  });
+  
+  promisifyFunc.catch(function (err) {
+    console.log(err);
+  });
+
+
+//now see how this complex syntax get simplified with async await ->
+
+// async await is syntactical sugar for promises 
+// async/await is promises with easy syntax
+
+
+//write async before function to make it return promises
+// async function f() {
+//     return "hello";
+//   }
+// console.log ( f() ) ; //return Promise { 'hello' }
+
+
+///using async keyword
+async function f() {
+    return "hello";
+  }
+  
+  //receive promise
+  let asyncf = f();
+  
+  //resolving promise by .then 
+  asyncf.then(function (data) {
+    console.log(data);
+  });
+  
+  asyncf.then(alert); // alert the returned message -> hello
+
+
+  //AWAIT
+//The keyword await makes JavaScript wait until that promise settles and returns its result.
+//The function execution “pauses” at the line 69 and resumes when the promise settles, with "result" variable
+//await literally suspends the function execution until the promise settles, and then resumes it with the promise result. That doesn’t cost any CPU resources, because the JavaScript engine can do other jobs in the meantime.
+//await waits for promise settlement so, it means it takes care of both .then and .catch
+
+  async function f(){
+      //here for experimenting we are making our own promise as we want this async function to return promise
+      let promise = new Promise(function(resolve, reject){
+        //setTimeout is async function so need cb or promises to resolve it  
+        //setTimeout(cb,timeInMilis) // syntax
+        setTimeout(function(){
+            resolve("done!");
+          }, 5000);
+
+        });
+
+        let result = await promise; // wait until the promise resolves
+        console.log("hello"); // this will run after 5sec because await stops everything in this function till promise get resolve/settled :) 
+        alert(result); // this alert will happen just after printing hello
+    
+  }
+
+f();
+console.log("yeahh");  //it wont wait for await as its not in function which contain await // not present after await in that function
+
+
+/**
+ * output:
+ * yeahh
+ * hello  //after 5sec
+ * done alert //eaxctly after hello printed
+ */
