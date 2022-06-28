6097f710b173028d60f1dfecb8def7f8b4a10e68
jyotijauhari <jyotijauhari222@gmail.com>
2022-06-28 22:31:59 +0530

    named import and export

--- Theory/React/reactBasics.js
+++ Theory/React/reactBasics.js
@@ -6,3 +6,12 @@ import yolo from "./temp";
 
 console.log(obj);
 console.log(yolo);
+
+//named imports
+import {key as haathi} from './strings';
+import { exportedObj } from './strings';
+
+//importing all
+import * as bundled from "./strings";
+bundled.key;
+bundled.exportedObj;
---
+++ Theory/React/strings.js
@@ -0,0 +1,6 @@
+// named export 
+export const key = "sha-123";
+export const exportedObj = {
+    name :"Uku",
+    age: 9
+}
