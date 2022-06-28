e9f9f0a80bd601eb061f8765b975e25ca93820bc
jyotijauhari <jyotijauhari222@gmail.com>
2022-06-28 22:28:38 +0530

    default import and export

---
+++ Theory/React/reactBasics.js
@@ -0,0 +1,8 @@
+//default imports
+
+import obj from './temp';
+import yolo from "./temp";
+
+
+console.log(obj);
+console.log(yolo);
---
+++ Theory/React/temp.js
@@ -0,0 +1,11 @@
+var obj = {
+    add : function add(a,b){
+        return a+b;
+    },
+
+    hello : 123
+}
+
+var a = 1000;
+
+export default obj;
