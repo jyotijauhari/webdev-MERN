15d16910ae0f9b895a64d46eb0b2fe2c17dc6322
jyotijauhari <jyotijauhari222@gmail.com>
2022-06-28 21:57:05 +0530

    revising old js concepts

---
+++ Theory/revision.js
@@ -0,0 +1,33 @@
+// // var function scoped
+// var a;
+// function koma(){
+//     var a = 20;
+//     console.log(a);
+// }
+
+// koma();
+// console.log(a);
+
+
+// function koma(){
+//     var a = 20;
+//     console.log(a);
+// }
+// console.log(koma());
+
+
+//how js works
+var e;
+let m = 50;
+function koma(){
+    var a = 20;
+    console.log(a);
+    console.log(m);
+}
+
+koma();
+var b = 800;
+var c = 90;
+let d = 80;
+// console.log(a);
+
