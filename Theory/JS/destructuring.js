5c201c1df578abf9cbd37df7ac212364a906e2f1
jyotijauhari <jyotijauhari222@gmail.com>
2022-06-30 21:17:23 +0530

    destructuring, oop, spread rest

---
+++ Theory/JS/destructuring.js
@@ -0,0 +1,17 @@
+//array
+[a, b] = ["FJP", 5];
+console.log(a); //fjp
+console.log(b); //5
+
+[n1, , n3] = [1, 2, 3];
+console.log(n1);
+console.log(n3);
+
+//object destructuring
+let obj = {
+    name: "Kiana",
+    age:2
+}
+
+let { age } = obj;
+console.log(age);
