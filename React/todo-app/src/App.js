b9636c3f4053b9aad2735bf8a69a7252fca5a7d0
jyotijauhari <jyotijauhari222@gmail.com>
2022-07-03 00:21:31 +0530

    todo-app started

---
+++ React/todo-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Component/Todo';
+
+function App() {
+  return (
+    // <h1>Hello from other side</h1>
+    <Todo/>
+  );
+}
+
+export default App;
